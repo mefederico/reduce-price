6134e63c4119bdf2a185eda21046c34cfe693c30
mefederico <federico.medina@pomelo.la>
2025-03-16 22:50:57 +0000

    Add config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mefederico.github.io',  // Reemplaza "tu-usuario" con tu nombre de usuario de GitHub
+  base: '/reduce-price',  // Reemplaza con el nombre de tu repositorio (o elimina esta línea si usas un dominio personalizado)
   vite: {
     css: {
       devSourcemap: true,
